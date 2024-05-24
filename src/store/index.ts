58c0a47266f900e1c65010402a258bd51898ab75
Dima M <54527086+dmitry-tupelo@users.noreply.github.com>
2024-05-25 00:19:39 +0300

    add redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import weatherReducer from "./weatherReducer";
 import citiesReducer from "./citiesReducer";
 import searchReducer from "./searchReducer";
 
-export const store = configureStore({
-  reducer: {
-    weather: weatherReducer,
-    search: searchReducer,
-    cities: citiesReducer,
-  },
+const rootReducer = combineReducers({
+  weather: weatherReducer,
+  search: searchReducer,
+  cities: citiesReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMidleware) =>
+    getDefaultMidleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
