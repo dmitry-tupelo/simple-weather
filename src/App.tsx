d1af8670b11efb2e67d2e07d89af57afc82f5d61
Dima M <54527086+dmitry-tupelo@users.noreply.github.com>
2024-05-26 19:14:44 +0300

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import { useSelector } from "react-redux";
 import CitiesList from "./components/CitiesList";
 
 function App() {
-  const weather = useSelector((state: any) => state.weather.forecast);
   const cities = useSelector((state: any) => state.cities);
 
   return (
     <div className="max-w-sm mx-auto">
       <WeatherSearch />
-      {weather ? (
-        <div className="w-full">
-          <WeatherCard
-            weather={weather?.location?.name}
-            isSearchResult={true}
-          />
-        </div>
-      ) : null}
       {cities.length > 0 && <CitiesList />}
     </div>
   );
--- src/components/WeatherSearch.tsx
+++ src/components/WeatherSearch.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchText } from "../store/searchReducer";
 import { saveWeather } from "../store/weatherReducer";
+import WeatherCard from "./WeatherCard";
 
 const API_KEY = "6155f29853fa4b96a0194442241705";
 
 const WeatherSearch = (props: any) => {
   const searchValue = useSelector((state: any) => state.search.value);
+  const weather = useSelector((state: any) => state.weather.forecast);
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -15,6 +18,7 @@ const WeatherSearch = (props: any) => {
   };
 
   const fetchAPI = () => {
+    setLoading(true);
     // Make a GET request
     fetch(
       `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${searchValue}`
@@ -31,22 +35,39 @@ const WeatherSearch = (props: any) => {
       })
       .catch((error) => {
         console.error("Error:", error);
-      });
+      })
+      .finally(() => setLoading(false));
   };
 
   useEffect(() => {
-    fetchAPI();
+    if (searchValue.length > 3) {
+      fetchAPI();
+    }
   }, [searchValue]);
 
   return (
-    <div className="flex flex-col items-center my-4">
+    <div className="flex flex-col items-center">
       <input
-        className="p-2 border-2 rounded-md border-stone-300 capitalize"
+        className="p-2 border-2 rounded-md border-stone-300 capitalize my-4"
         value={searchValue}
         onChange={handleCityChange}
         type="text"
         placeholder="Type your city"
       />
+      {!loading ? (
+        <div>
+          {weather ? (
+            <div className="w-full">
+              <WeatherCard
+                weather={weather?.location?.name}
+                isSearchResult={true}
+              />
+            </div>
+          ) : null}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
